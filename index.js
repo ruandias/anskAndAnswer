57753d654e2b7574148080d836d9dd8754bcc355
ruanndias <ruan_ndias@hotmail.com>
2020-06-09 12:34:14 -0300

    created ask route

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-  res.send("Bem vindo ao meu site!");
+  res.render('index');
+});
+
+app.get("/ask", (req, res) => {
+  res.render('ask');
 });
 
 app.listen(8080, () => {
