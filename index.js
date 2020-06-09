27f38e4eafcc857fec94ef257f736a0c1e946f70
ruanndias <ruan_ndias@hotmail.com>
2020-06-09 13:39:36 -0300

    saving questions on console log

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render('index');
@@ -15,7 +18,12 @@ app.get("/ask", (req, res) => {
 });
 
 app.post("/savequestion", (req,res) => {
-  res.send("Received form");
+  const question = {
+    title: req.body.title,
+    describe: req.body.describe
+  };
+  console.log(question);
+  res.send(`${question.title} and ${question.describe}`);
 })
 
 app.listen(8080, () => {
